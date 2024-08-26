4ddcc9403e06585149230b5dcda1417f6778d48d
joppe <joppe@apestaartje.info>
2024-08-26 19:05:05 +0200

    chore: nicer demo

--- src/app/Info.tsx
+++ src/app/Info.tsx
@@ -4,25 +4,32 @@ import { Title } from './Title';
 import { useState } from '@/jsx/hook';
 
 export function Info(): JSX.Element {
-    const title = 'list';
+    const title = 'Counter';
     const [counter, setCounter] = useState(0);
 
-    function handleClick() {
+    function increment() {
         setCounter(counter + 1);
+    }
+
+    function decrement() {
+        setCounter(counter - 1);
     }
 
     return (
         <>
             <div data-test="root">
-                <Title title={title} />
+                <Title title={title} count={counter} />
                 <p>Lorem</p>
                 <ul className="foo">
                     <li>first</li>
                     <li>2</li>
                     <li>third</li>
                 </ul>
-                <p>{counter}</p>
-                <button onClick={handleClick}>Click me</button>
+                <div style="display: flex; gap: 10px">
+                    <button onClick={decrement}>-</button>
+                    {counter}
+                    <button onClick={increment}>+</button>
+                </div>
             </div>
         </>
     );
